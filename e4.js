711bde0c922a45640990efeb8ce9705dafd93c2b
DevMasiero <andremasierodev@gmail.com>
2024-09-02 20:17:16 -0300

    resolucao-exercicio-04

---
+++ e4.js
@@ -0,0 +1,5 @@
+var frase = 'teste 1 de 2 string 3';
+
+var Sem_num = frase.replace(/\d+/g, '[removido]');
+
+console.log(Sem_num);
---
+++ readme.md
@@ -0,0 +1,2 @@
+Dado a seguinte string 'teste 1 de 2 string 3', substitua todas as ocorrências
+de números pelo valor '[removido]'.
